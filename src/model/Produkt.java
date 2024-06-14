3b9d085c734b49ab625ed5a6bfa9d992ae786378
Barschbart <104435545+Barschbart@users.noreply.github.com>
2024-06-14 12:42:13 +0200

    Test

---
+++ src/model/Produkt.java
@@ -0,0 +1,25 @@
+package model;
+
+import jakarta.persistence.Entity;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class Produkt {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int pID;
+
+    private String prodName;
+    private String description;
+    private String unit;
+
+    @OneToMany(mappedBy = "produkt")
+    private Set<Rabatteintragung> rabatteintrags;
+
+    @OneToMany(mappedBy = "produkt")
+    private Set<Tankung> tankungs;
+
+    // Getters and setters
+}
